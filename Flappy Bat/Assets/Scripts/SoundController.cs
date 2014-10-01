04d219b95694520c3c779edcdf53eb14e97c6926
Juan Antonio Fajardo Serrano <juan61088@gmail.com>
2014-10-01 20:22:52 +0200

    Added: Mute/Unmute button in main menu screen

---
+++ Flappy Bat/Assets/Scripts/SoundController.cs
@@ -0,0 +1,83 @@
+﻿/*
+Copyright 2014 Juan Antonio Fajardo Serrano
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+
+using UnityEngine;
+using System.Collections;
+
+public class SoundController : MonoBehaviour {
+
+	static public bool sound_on = true;
+
+	public bool isMute = false;
+	public bool isUnmute = false;
+	
+	private GameObject mute;
+	private GameObject unmute;
+
+	void Start () {
+		mute = GameObject.Find("Mute");
+		unmute = GameObject.Find("Unmute");
+		
+		Vector3 pos_mute = new Vector3();
+		pos_mute = mute.transform.position;
+		
+		Vector3 pos_unmute = new Vector3();
+		pos_unmute = unmute.transform.position;
+		
+		if (sound_on) { // If sound is on
+			// Put mute icon
+			pos_mute.z = 0;
+			pos_unmute.z = 10;
+		}
+		else { // If sound is off
+			// Put unmute icon
+			pos_mute.z = 10;
+			pos_unmute.z = 0;
+		}
+		
+		mute.transform.position = pos_mute;
+		unmute.transform.position = pos_unmute;
+	}
+
+	void OnMouseUp () {
+
+		mute = GameObject.Find("Mute");
+		unmute = GameObject.Find("Unmute");
+
+		Vector3 pos_mute = new Vector3();
+		pos_mute = mute.transform.position;
+
+		Vector3 pos_unmute = new Vector3();
+		pos_unmute = unmute.transform.position;
+
+		if (sound_on) { // If sound was on
+			// Put unmute icon
+			pos_mute.z = 10;
+			pos_unmute.z = 0;
+		}
+		else { // If sound was off
+			// Put mute icon
+			pos_mute.z = 0;
+			pos_unmute.z = 10;
+		}
+
+		mute.transform.position = pos_mute;
+		unmute.transform.position = pos_unmute;
+
+		sound_on = !sound_on; // Change state
+	}
+}
